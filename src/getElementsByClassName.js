b9ad46a4ef9d8c2c3358d6f91f27f5d4c0e01b74
seanlangbrown <seanlangbrown@gmail.com>
2018-01-15 12:32:34 -0800

    implemented getElementsByClassName, incomplete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  var results = [];
+  node = node || document.body;
+
+  if (node.classList.contains(className)) {
+    results.push(node);
+  }
+  
+
+  if (node.childNodes.length) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      results.concat(getElementsByClassName(className, node.childNodes[i]));
+    }
+    
+  }
+
+  
+  return results;
+  
+
 };
